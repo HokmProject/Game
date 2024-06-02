3d56a335290360588cee944e4a12760bf8899d65
Nima <Nimafeli138384@gmail.com>
2024-06-02 10:26:58 +0330

    some parts of login page and score part

---
+++ src/main/java/UserInterface/Score.java
@@ -0,0 +1,27 @@
+package userInterface;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Score {
+
+    static String points, points2, rounds, rounds2;
+    static void score(JFrame frame) {
+        points = points2 = rounds2 = rounds = "0";
+        JLabel team1score= new JLabel("team 1: "+points+"                                               rounds: "+rounds);
+        team1score.setBounds(10, 600, 400, 40);
+        team1score.setBorder(BorderFactory.createMatteBorder(1, 1, 1, 1, Color.black));
+        frame.add(team1score);
+
+        JLabel team2score = new JLabel("team 2: "+points2+"                                               rounds: "+rounds2);
+        team2score.setBounds(10, 650, 400, 40);
+        team2score.setBorder(BorderFactory.createMatteBorder(1, 1, 1, 1, Color.black));
+        frame.add(team2score);
+
+        JLabel ID = new JLabel("Group ID: ");
+        ID.setBounds(450, 610, 300, 70);
+        ID.setFont(new Font("Arial", Font.BOLD, 30));
+        frame.add(ID);
+    }
+
+}
